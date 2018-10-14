fd77047be9aea90e0a8ee88720adc53a5b08196e
David Inoa <davidinoa@gmail.com>
2018-10-14 11:07:20 -0400

    rename, rearrange files

---
+++ array-string-manipulation/reverse-words.js
@@ -0,0 +1,21 @@
+function reverseWords(message) {
+  reverseCharacters(message, 0, message.length - 1);
+
+  let currentWordStartIndex = 0;
+  for (let i = 0; i < message.length; i++) {
+    if (i === message.length || message[i] === ' ') {
+      reverseCharacters(message, currentWordStartIndex, i - 1);
+      currentWordStartIndex = i + 1;
+    }
+  }
+}
+
+function reverseCharacters(message, leftIndex, rightIndex) {
+  while (leftIndex < rightIndex) {
+    const temp = message[leftIndex];
+    message[leftIndex] = message[rightIndex];
+    message[rightIndex] = temp;
+    leftIndex++;
+    rightIndex--;
+  }
+}
---
+++ hashing-hashtables/word-cloud.js
@@ -0,0 +1,6 @@
+"After beating the eggs, Dana read the next step:"
+"Add milk and eggs, then add flour and sugar."
+
+function isLetter(character) {
+  return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(character) >= 0;
+}
